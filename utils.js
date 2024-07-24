570d9585e52de1ea990628177a5a3f85a32879b2
mechanik-daniel <daniel@outburn.co.il>
2024-07-24 19:40:00 +0300

    ruleset generation from sushi-config

--- utils.js
+++ utils.js
@@ -61,6 +61,15 @@ export const getFshOutputFolder = () => {
     return path.join(workingDir, 'fsh-generated', 'resources');
 };
 
+export const getFshInputFolder = () => {
+    return path.join(workingDir, 'input', 'fsh');
+};
+
+export const overwriteRuleSet = (fsh) => {
+    const fshFile = path.join(getFshInputFolder(), 'RuleSet-metadata.fsh');
+    return fs.writeFileSync(fshFile, fsh);
+}
+
 export const getExamplesFolder = () => {
     return path.join(workingDir, 'examples');
 };
